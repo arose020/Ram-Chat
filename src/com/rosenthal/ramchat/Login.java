091938ba23a831b4f08e1234bd34e3411a0f9177
Andrew Rosenthal <rosenthal.andrew@att.net>
2013-08-04 14:41:18 -0500

    Created login class and GUI

---
+++ src/com/rosenthal/ramchat/Login.java
@@ -0,0 +1,96 @@
+package com.rosenthal.ramchat;
+
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+
+public class Login extends JFrame {
+	private static final long serialVersionUID = 1L;
+	
+	private JPanel contentPane;
+	private JTextField txtUsername;
+	private JTextField txtIpAddress;
+	private JTextField txtPort;
+	private JLabel lblIpAddress;
+	private JLabel lblPort;
+	private JLabel lblExampleIp;
+	private JLabel lblExamplePort;
+
+	public Login() {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		setAlwaysOnTop(true);
+		setResizable(false);
+		setTitle("Login to Ram Chat");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 290);
+		setLocationRelativeTo(null);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtUsername = new JTextField();
+		txtUsername.setBounds(64, 35, 165, 28);
+		contentPane.add(txtUsername);
+		txtUsername.setColumns(10);
+		
+		JLabel lblEnterUsername = new JLabel("Choose Username:");
+		lblEnterUsername.setBounds(99, 11, 96, 14);
+		contentPane.add(lblEnterUsername);
+		
+		txtIpAddress = new JTextField();
+		txtIpAddress.setColumns(10);
+		txtIpAddress.setBounds(64, 98, 165, 28);
+		contentPane.add(txtIpAddress);
+		
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(64, 170, 165, 28);
+		contentPane.add(txtPort);
+		
+		lblIpAddress = new JLabel("IP Address:");
+		lblIpAddress.setBounds(119, 82, 56, 14);
+		contentPane.add(lblIpAddress);
+		
+		lblPort = new JLabel("Port:");
+		lblPort.setBounds(132, 155, 29, 14);
+		contentPane.add(lblPort);
+		
+		lblExampleIp = new JLabel("(Example: 192.168.0.2)");
+		lblExampleIp.setBounds(87, 130, 120, 14);
+		contentPane.add(lblExampleIp);
+		
+		lblExamplePort = new JLabel("(Example: Port: 8123)");
+		lblExamplePort.setBounds(93, 204, 108, 14);
+		contentPane.add(lblExamplePort);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setBounds(102, 229, 89, 23);
+		contentPane.add(btnLogin);
+	}
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Login frame = new Login();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
